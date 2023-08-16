c5cb0fc38e32e8769139cb47e3f3602bf9e5ec75
Lukasz17git <lucas.m2295@gmail.com>
2023-08-16 23:56:35 +0200

    testing nuxt app

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+      <p>Hello from about page</p>
+   </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+   setup() {
+
+
+      return {}
+   }
+})
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+      <h1>Hello from home page</h1>
+   </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+   setup() {
+
+
+      return {}
+   }
+})
+</script>
+
+<style scoped></style>
